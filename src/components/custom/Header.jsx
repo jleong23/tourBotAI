b57c6fb76bec2c1275c0f7366c65c2600abd664c
Jason Leong <jleongdh@gmail.com>
2026-01-06 17:41:36 +1030

    Updated title

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import {Button} from '../ui/button.jsx'
+import React from "react";
+import { Button } from "../ui/button.jsx";
 
 const Header = () => {
   return (
-    <div className='w-full p-3 shadow-sm flex justify-between items-center px-5'>
-       <h1 className='font-semibold'>
-          <span className='text-orange-500'>Your</span>Planner
-        </h1>
+    <div className="w-full p-3 shadow-sm flex justify-between items-center px-5">
+      <h1 className="font-semibold">
+        <span className="text-orange-500">TourBot</span>AI
+      </h1>
       <div>
         <Button iconSize="lg" className="text-base">
           Sign In
         </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
